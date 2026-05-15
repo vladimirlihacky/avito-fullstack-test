907f767afebcc87002e1e3c8a212cb873d76c5dc
vladimirlihacky <vladimirlihacky@gmail.com>
2026-05-16 01:44:10 +0300

    fix: k6 ci

--- backend/tests/load/test.js
+++ backend/tests/load/test.js
@@ -13,7 +13,7 @@ export const options = {
   },
 };
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080'
 const ASSISTANT_ID = '00000000-0000-0000-0000-000000000004';
 
 export function setup() {
